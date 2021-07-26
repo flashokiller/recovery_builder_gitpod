98038bee2cf2a0c9edb54784b0eeb0d6494db6f4
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-07-26 08:58:57 +0530

    Update build_recovery.sh

--- build_recovery.sh
+++ build_recovery.sh
@@ -10,12 +10,13 @@ JOBS=nproc
 # Set up git!
 git config --global user.name ${GITHUB_USER}
 git config --global user.email ${GITHUB_EMAIL}
-7
 # make directories
 mkdir ${WORK_DIR} && cd ${WORK_DIR}
 git clone https://gitlab.com/OrangeFox/sync.git
 cd ~/ORANGE_FOX/sync
+export USE_SSH=1
 ./get_fox_10.sh ~/ORANGEFOX/fox_10.0
+cd .. && cd ~/ORANGEFOX/fox_10.0
 
 #device
 git clone --depth=1  https://github.com/flashokiller/twrp_micromax_e6746 device/micromax/E6746
@@ -24,4 +25,10 @@ git clone --depth=1  https://github.com/flashokiller/twrp_micromax_e6746 device/
 #git clone --depth=1 https://github.com/GengKapak/GengKapak-clang --single-branch -b 13 prebuilts/clang/host/linux-x86/clang-gengkapak
 
 # Start building!
-. sync/build_fox.sh
+. build/envsetup.sh
+lunch omni_E6746-eng
+export OF_VANILLA_BUILD=1
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export LC_ALL="C"
+make recoveryimage
